bfe6a1424874c6052e7763231836806a844dd291
jawzal19-meet <jawzal19@meet.mit.edu>
2017-11-06 19:23:02 +0200

    I updated my code!

---
+++ lab4.py
@@ -0,0 +1,21 @@
+class Animal(object):
+	def__init__(self,sound,name,age,favorite_color)
+		self.sound=sound
+		self.name=name
+		self.age=age
+		self.favorite_color=favorite_color
+	def eat(self,food):
+		print("Yummy!! "+self.name+"is eating "+food)
+	def discription(self):
+		print(self.name+"is"+self.age+"years old and loves the color "+self,favorite_color)
+	def make_sound(self)
+		print(self.sound*3)
+	def sound_times(self,times)
+		print(self.sound*times)
+cat=Animal("Meow","sugar",1,"white")
+cat.eat("fish")
+cat.discription()
+
+
+class Person(self,name,age,ciry,gender)
+
